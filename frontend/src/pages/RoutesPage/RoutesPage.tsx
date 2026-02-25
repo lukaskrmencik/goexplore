a8659617719f3711afabf85cf1759bbbdbcc7284
lukaskrmencik <krmencik.lukas@gmail.com>
2026-02-25 00:35:36 +0000

    refactor: Optimize invite system logic and enhance code readability

---
+++ frontend/src/pages/RoutesPage/RoutesPage.tsx
@@ -0,0 +1,7 @@
+import RoutesListEditor from "../../features/editors/routesListEditor/RoutesListEditor/RoutesListEditor";
+
+const RoutesPage = () => {
+    return <RoutesListEditor />;
+};
+
+export default RoutesPage;
